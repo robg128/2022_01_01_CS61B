bdf60f17a28d41c33e36f9a5db62a88d70de1c23
hiyoyolumi <862127279@qq.com>
2022-01-06 01:15:22 +0800

    proj0_Begin_ReadRadius

---
+++ proj0/Planet.java
@@ -0,0 +1,97 @@
+public class Planet {
+    double xxPos;
+    double yyPos;
+    double xxVel;
+    double yyVel;
+    double mass;
+    String imgFileName;
+
+    static double G = 6.67e-11;
+
+    public Planet(double xxPos, double yyPos, double xxVel,
+                  double yyVel, double mass, String imgFileName) {
+        this.xxPos = xxPos;
+        this.yyPos = yyPos;
+        this.xxVel = xxVel;
+        this.yyVel = yyVel;
+        this.mass = mass;
+        this.imgFileName = imgFileName;
+    }
+
+    public Planet(Planet p) {
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
+    }
+
+    /** Calculate the Distance between this and Planet p2*/
+    public double calcDistance(Planet p2) {
+        double xtempx = Math.abs(xxPos - p2.xxPos);
+        double ytempy = Math.abs(yyPos - p2.yyPos);
+
+        return Math.sqrt((xtempx * xtempx) + (ytempy * ytempy));
+    }
+
+    /** Calculate the Force between this and p2 */
+    public double calcForceExertedBy(Planet p2) {
+        double rSquare = Math.pow(this.calcDistance(p2), 2);
+        
+        return (G * this.mass * p2.mass) / rSquare;
+    }
+
+    /** Calculate the Component in the X direction of the Force */
+    public double calcForceExertedByX(Planet p2) {
+        double dx = p2.xxPos - xxPos;
+        double F = this.calcForceExertedBy(p2);
+
+        return (F * dx) / this.calcDistance(p2);
+    }
+
+    /** Calculate the Component in the Y direction of the Force */
+    public double calcForceExertedByY(Planet p2) {
+        double dy = p2.yyPos - yyPos;
+        double F = this.calcForceExertedBy(p2);
+
+        return (F * dy) / this.calcDistance(p2);
+    }
+
+    /** Calculate the net Force in the X direction of the Net Force */
+    public double calcNetForceExertedByX(Planet[] array) {
+        double XNetForce = 0;
+        for (int i = 0; i < array.length; i++) {
+            /** If array[i] as same as this, then skip */
+            if (this.equals(array[i]) == true) {
+                continue;
+            }
+            XNetForce += this.calcForceExertedByX(array[i]);
+        }
+        return XNetForce;
+    }
+
+    /** Calculate the net Force in the Y direction of the Net Force */
+    public double calcNetForceExertedByY(Planet[] array) {
+        double YNetForce = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (this.equals(array[i]) == true) {
+                continue;
+            }
+            YNetForce += this.calcForceExertedByY(array[i]);
+        }
+        return YNetForce;
+    }
+
+    /** Update the planet's position and velocity instance variables */
+    public void update(double t, double fX, double fY) {
+        double aNetX = fX / this.mass;
+        double aNetY = fY / this.mass;
+
+        this.xxVel = this.xxVel + aNetX * t;
+        this.yyVel = this.yyVel + aNetY * t;
+
+        this.xxPos = this.xxPos + this.xxVel * t;
+        this.yyPos = this.yyPos + this.yyVel * t;
+    }
+}
